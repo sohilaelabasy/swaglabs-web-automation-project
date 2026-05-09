26d986e0ab5c0556fef397171c4cd2acdd9ff7a0
sohila elabasy <sohailaismail6@gmail.com>
2026-05-09 16:54:58 +0300

    Add login test cases for valid and invalid login scenarios

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,12 +1,100 @@
 package testcases;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import utilities.DriverFactory;
+import pages.LandingPage;
+import pages.LoginPage;
 
 public class LoginTest extends BaseTest {
+    LoginPage loginPage;
+    @BeforeMethod
+    public void setupPage(){
+        loginPage = new LoginPage(driver);
+    }
+    //Test Case 1 : Valid Login with standard_user(positive tc)
     @Test
-    public void openLoginPage() {
-        driver = DriverFactory.getDriver("chrome");
-        driver.getCurrentUrl();
+    public void validLogin(){
+        loginPage.login("standard_user" ,"secret_sauce");
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+    }
+
+    //Test Case 2 : Login with locked_out_user(negative tc)
+    @Test
+    public void lockedOutUserLogin(){
+        loginPage.login("locked_out_user" , "secret_sauce");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Sorry, this user has been locked out"));
+    }
+
+    //Test Case 3 : Login with invalid username(negative tc)
+    @Test
+    public void invalidUsernameLogin(){
+        loginPage.login("invalid_user" , "secret_sauce");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Username and password do not match"));
+    }
+
+    //Test Case 4 : Login with invalid password(negative tc)
+    @Test
+    public void invalidPasswordLogin(){
+        loginPage.login("standard_user", "wrong_password");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Username and password do not match"));
+    }
+
+    //Test Case 5: Login with empty username(negative tc)
+    @Test
+    public void emptyUsername(){
+        loginPage.login("" , "secret_sauce");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Username is required"));
+    }
+
+    //Test Case 6: Login with empty password(negative tc)
+    @Test
+    public void emptyPassword(){
+        loginPage.login("standard_user", "");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Password is required"));
+    }
+
+    //Test Case 7: Login with empty username and password(negative tc)
+    @Test
+    public void emptyUsernameAndPassword(){
+        loginPage.login("", "");
+        Assert.assertTrue(loginPage.getErrorMessage()
+                .contains("Username is required"));
+    }
+
+    //Test Case 8: Login with problem_user
+    @Test
+    public void loginWithProblemUser(){
+        loginPage.login("problem_user" , "secret_sauce");
+        Assert.assertTrue(driver.getCurrentUrl()
+                .contains("inventory"));
+    }
+
+    //Test Case 9: Verify Login Page Title(positive tc )
+    @Test
+    public void verifyLoginPageTitle(){
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+    }
+
+    //Test case 10 : Login with performance_glitch_user(positive tc)
+    @Test
+    public void loginWithPerformanceGlitchUser() {
+        loginPage.login("performance_glitch_user", "secret_sauce");
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+    }
+
+    //Test case 11 : Logout after login(positive tc)
+    @Test
+    public void logoutAfterLogin() {
+        loginPage.login("standard_user", "secret_sauce");
+        LandingPage landingPage = new LandingPage(driver);
+
+        loginPage = landingPage.clickLogout();
+        Assert.assertTrue(driver.getCurrentUrl().contains("saucedemo"));
     }
 }
